0e842b9030714e2baa364882fdd56a81798f0aac
sferrazzaf <francis.sferrazza@gmail.com>
2022-11-28 10:16:30 -0500

    Add background image, prompt, and text input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <main className="App-main">
+        <p className="quip">"What have you forgotten today?"</p>
+        <input type="text"></input> 
       </main>
     </div>
   );
